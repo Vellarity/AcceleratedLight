cae363761c75c97ff2cf51a6380df2ce7a737f5c
Balduev Nikita <Nikitaaorta@ya.ru>
2026-01-23 12:10:06 +0300

    WIP: Собираем state через combine и stateIn, чтобы не возиться с синхронизацией значений во ViewModel и репозиториях

--- app/src/main/java/com/vellarity/lightaccs/ui/screen/MainScreenState.kt
+++ app/src/main/java/com/vellarity/lightaccs/ui/screen/MainScreenState.kt
@@ -1,7 +1,7 @@
 package com.vellarity.lightaccs.ui.screen
 
 data class MainScreenState(
-    val isLight: Boolean,
-    val isServiceActive: Boolean,
+    val isLight: Boolean = false,
+    val isServiceActive: Boolean = false,
     val accelerationThreshold: Float = 12.0f,
 )
--- app/src/main/java/com/vellarity/lightaccs/ui/screen/MainScreenViewModel.kt
+++ app/src/main/java/com/vellarity/lightaccs/ui/screen/MainScreenViewModel.kt
@@ -1,24 +1,14 @@
 package com.vellarity.lightaccs.ui.screen
 
-import android.app.Application
-import android.content.Context
-import android.content.Intent
-import android.graphics.Camera
-import android.hardware.camera2.CameraManager
-import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.vellarity.lightaccs.data.interactor.FlashlightInteractor
 import com.vellarity.lightaccs.data.repository.FlashlightRepository
 import com.vellarity.lightaccs.data.repository.SettingsRepository
-import com.vellarity.lightaccs.data.service.LightAcceleratorService
 import com.vellarity.lightaccs.data.service.SystemVibratorManager
 import com.vellarity.lightaccs.data.usecase.InvokeServiceUseCase
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
 class MainScreenViewModel(
     private val flashlightRepository: FlashlightRepository,
@@ -26,27 +16,22 @@ class MainScreenViewModel(
     private val vibratorManager: SystemVibratorManager,
     private val invokeServiceUseCase: InvokeServiceUseCase
 ): ViewModel() {
-    private var _state: MutableStateFlow<MainScreenState> = MutableStateFlow(
+
+    val state = combine(
+        flashlightRepository.isFlash,
+        settingsRepository.isServiceActive,
+        settingsRepository.accelerateThreshold) {
+        isFlash, isServiceActive, accelerateThreshold ->
         MainScreenState(
-            isLight = false,
-            isServiceActive = false,
-            accelerationThreshold = 12f
+            isLight = isFlash,
+            isServiceActive = isServiceActive,
+            accelerationThreshold = accelerateThreshold
         )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = MainScreenState()
     )
-    val state = _state.asStateFlow()
-
-    init {
-        viewModelScope.launch {
-            settingsRepository.isServiceActive.collect {
-                _state.value.copy(isServiceActive = it)
-            }
-
-            settingsRepository.accelerateThreshold.collect {
-                _state.value.copy(accelerationThreshold = it)
-            }
-        }
-        _state.value.copy(isLight = flashlightRepository.isFlash.value)
-    }
 
     fun onAction(action: MainScreenAction) {
         when (action) {
@@ -58,8 +43,5 @@ class MainScreenViewModel(
     private fun toggleLight() {
         flashlightRepository.toggleFlash(!flashlightRepository.isFlash.value)
         vibratorManager.vibrate(400, 150)
-        _state.value = _state.value.copy(
-            isLight = flashlightRepository.isFlash.value
-        )
     }
 }
